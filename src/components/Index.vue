9fabbbcd0c9b91a842cffe8e7236697e48f21222
caishijian2008 <442754043@qq.com>
2018-03-09 00:20:28 +0800

    for index

---
+++ src/components/Index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <router-link to="/#">$ref, $nextTick()等</router-link>
+    <!--<router-view/>-->
+  </div>
+</template>
+
+<script>
+export default {
+  //
+}
+</script>
+
+<style scoped>
+a {
+  padding: 5px;
+  background: crimson;
+  color: #fff;
+  text-decoration: none;
+}
+</style>
