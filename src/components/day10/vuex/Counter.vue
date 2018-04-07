9dc3bcaaa4df5e3a3360c5e58085cfb19b5ad9d4
caishijian2008 <442754043@qq.com>
2018-04-07 15:43:53 +0800

    mutations

--- src/components/day10/vuex/Counter.vue
+++ src/components/day10/vuex/Counter.vue
@@ -3,15 +3,24 @@
     <p>vuex用于：平级组件交互，找共同的父级解决，跨组件交互，解决EventBus的混乱（基于发布订阅模式）</p>
     <p>vuex是为了大型项目开发，主要是<b>状态</b>管理，将数据统一管理</p>
     <p>状态就是数据，相当于组件中的data()函数（不完全理解）</p>
-    计数器：<button>-</button><br />
-    当前：<br />
-    计数器：<button>+</button>
+    计数器：<button @click="minus">-</button><br />
+    当前：{{this.$store.state.count}}<br />
+    计数器：<button @click="add">+</button>
   </div>
 </template>
 
 <script>
 export default {
-  //
+  methods: {
+    add () {
+      // 提交add的mutation
+      // 载荷：payload
+      this.$store.commit('add', 2)
+    },
+    minus () {
+      //
+    }
+  }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -9,13 +9,28 @@ import Vuex from 'vuex'
 // vuex是为了大型项目开发，主要是状态管理，将数据统一管理
 // 状态就是数据，相当于组件中的data()函数（不完全理解）
 Vue.use(Vuex)
-
+const state = { count: 0 }
+const mutations = {
+  // state是自动放入的，默认指的就是当前的state
+  // num就是载荷（payload）
+  add (state, num) {
+    if (isNaN(parseInt(num))) return
+    state.count += num
+  },
+  minus () {}
+}
+const store = new Vuex.Store({ // 容器是唯一的
+  strict: true, // 只能通过mutation来更改状态，mutation相当于管理员的角色，mutation不支持异步
+  state,
+  mutations
+})
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store, // store被注册到了实例上，所有组件都会有一个属性：this.$store
   components: { App },
   template: '<App/>'
 })
