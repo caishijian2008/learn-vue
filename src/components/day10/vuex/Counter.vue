2020404b27ae302a3f686cab1fd559d1d32f4e32
caishijian2008 <442754043@qq.com>
2018-04-08 02:30:07 +0800

    Getter

--- src/components/day10/vuex/Counter.vue
+++ src/components/day10/vuex/Counter.vue
@@ -2,10 +2,11 @@
   <div>
     <p>vuex用于：平级组件交互，找共同的父级解决，跨组件交互，解决EventBus的混乱（基于发布订阅模式）</p>
     <p>vuex是为了大型项目开发，主要是<b>状态</b>管理，将数据统一管理</p>
-    <p>状态就是数据，相当于组件中的data()函数（不完全理解）</p>
+    <p>状态就是数据，相当于组件中的data()函数</p>
     计数器：<button @click="minus">-</button><br />
     当前：{{this.$store.state.count}}<br />
-    计数器：<button @click="add">+</button>
+    计数器：<button @click="add">+</button><br />
+    {{$store.getters.val}}
   </div>
 </template>
 
--- src/store/index.js
+++ src/store/index.js
@@ -5,9 +5,12 @@ import mutations from './mutations'
 
 // vuex用于：平级组件交互，找共同的父级解决，跨组件交互，解决EventBus的混乱（基于发布订阅模式）
 // vuex是为了大型项目开发，主要是状态管理，将数据统一管理
-// 状态就是数据，相当于组件中的data()函数（不完全理解）
+// 状态就是数据，相当于组件中的data()函数
 Vue.use(Vuex)
 const state = { count: 0 }
+const getters = { // getter相当于是组件的计算属性computed
+  val: (state) => state.count % 2 ? '奇数' : '偶数'
+}
 
 export default new Vuex.Store({ // 容器是唯一的
   // 不要在发布环境下启用严格模式!!
@@ -15,5 +18,6 @@ export default new Vuex.Store({ // 容器是唯一的
   strict: true, // 只能通过mutation来更改状态，mutation相当于管理员的角色，mutation不支持异步
   plugins: [logger()], // logger是vuex的日志插件
   state,
-  mutations
+  mutations,
+  getters
 })
